d418aef9daa4105fc3467a4f9a4192803032cd86
Pedro Samuel <pedrosviki1265@gmail.com>
2023-11-21 16:44:53 -0400

    aplicando a funcao renderCartItems

--- carrinho/script.js
+++ carrinho/script.js
@@ -3,4 +3,45 @@ function getCartItems() {
   return cartItems;
 }
 
+function renderCartItems() {
+  const cartItemsContainer = document.getElementById('cartItemsContainer');
+  const cartItems = getCartItems();
+
+  cartItemsContainer.innerHTML = '';
+
+  cartItems.forEach(item => {
+    const itemContainer = document.createElement('div');
+    itemContainer.classList.add('info-cart');
+
+    itemContainer.innerHTML = `
+      <div> 
+        <img src="${item.cryptoImg}" alt="${item.cryptoName}">
+        <i class="fas fa-trash-alt" onclick="removeCoins('${item.cryptoName}')"></i>
+      </div>
+      
+    `;
+
+    const title = document.createElement('p');
+    title.classList.add('title');
+    title.textContent = item.cryptoName;
+
+    const priceContainer = document.createElement('div');
+    priceContainer.classList.add('price-cart');
+
+    priceContainer.innerHTML = `
+      <p>${item.cryptoPrice}</p>
+      <div>
+        <button onclick="removeCoins('${item.cryptoName}')">-</button>
+        <span>1</span>
+        <button onclick="addMoreCoins('${item.cryptoName}')">+</button>
+      </div>
+    `;
+
+    itemContainer.appendChild(title);
+    itemContainer.appendChild(priceContainer);
+    cartItemsContainer.appendChild(itemContainer);
+  });
+
+  updateTotalPrice();
+}
 
